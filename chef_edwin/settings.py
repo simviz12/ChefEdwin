4d0014ee4b714e868497f6c7ff52996d457caf99
simviz12 <carlos.benavidesbu@gmail.com>
2025-12-16 17:08:13 -0500

    Temp fix: Allow all hosts for debugging

--- chef_edwin/settings.py
+++ chef_edwin/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fs@=rtgqcaczbdt_k(i02$8*p$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,*.onrender.com').split(',')
+ALLOWED_HOSTS = ['*']  # Temporarily allow all for debugging
 
 
 # Application definition

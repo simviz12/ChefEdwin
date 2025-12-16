2a941f556fa0155c44f36ea2ff6e6dd478add0a3
simviz12 <carlos.benavidesbu@gmail.com>
2025-12-16 17:18:36 -0500

    Debug: Add ping-root endpoint

--- chef_edwin/urls.py
+++ chef_edwin/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('assistant/', include('assistant.urls')),
+    path('ping-root/', lambda request: HttpResponse('pong-root'), name='ping_root'),
 ]
